41efe45129692f0edc8c05b6d6c513d0ddd9840e
Rodolfo Hernandez <rodolfohernan20@gmail.com>
2018-07-06 01:29:14 -0600

    Cambios a informacion de pais

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-            new SunCat\MobileDetectBundle\MobileDetectBundle(),
+          //  new SunCat\MobileDetectBundle\MobileDetectBundle(),
             new AppBundle\AppBundle(),
         ];
 
