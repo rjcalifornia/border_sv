7ecca5da80985234f2106b4fe84861ddb65e0943
Rodolfo Hernandez <rodolfohernan20@gmail.com>
2018-07-24 21:56:05 -0600

    Cambios en Codigos de Paises

--- src/AppBundle/Controller/CodigosController.php
+++ src/AppBundle/Controller/CodigosController.php
@@ -9,6 +9,18 @@ use AppBundle\Entity\Codigos;
 use AppBundle\Entity\Mapas;
 use AppBundle\Entity\Banderas;
 use AppBundle\Form\AgregarCodigoType;
+use Symfony\Component\Form\AbstractType;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;   
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
 
 class CodigosController extends Controller
 {
@@ -18,7 +30,68 @@ class CodigosController extends Controller
     public function AgregarAction(Request $request)
     {
         $pais = new Codigos();
-        $form = $this->createForm(AgregarCodigoType::class, $pais);
+       // $form = $this->createForm(AgregarCodigoType::class, $pais);
+        $codigosListadoRepository = $this->getDoctrine()->getRepository('AppBundle:Codigos');
+        
+        $codigosPaises = $codigosListadoRepository->createQueryBuilder("r")
+        ->select('IDENTITY(r.paisid)')
+        ->getQuery()
+        ->getResult();
+        
+        $paisesRepository = $this->getDoctrine()->getRepository('AppBundle:Paises');
+        if($codigosPaises != null)
+        {
+              $paises = $paisesRepository->createQueryBuilder("q")
+                     ->where("q.id NOT IN (:mispaises)")
+                     ->setParameter("mispaises", $codigosPaises)
+                     ->getQuery()
+                     ->getResult();
+        }
+
+        if($codigosPaises == null)
+        {
+            $paises = $paisesRepository->findAll();
+        }
+      
+         
+         $form = $this->createFormBuilder($pais);
+        $form->add('paisid', 
+              EntityType::class, 
+
+                        array(
+                            'class' => \AppBundle\Entity\Paises::class,
+                            'choice_label' => 'paisnombre',
+                            //'choice_value' => 'id',
+                             'choices' => $paises,
+                            'placeholder'=> 'Seleccione un pais',
+                            "attr" => array
+                            ('class' => 'select2 form-control col-md-7 col-xs-12')
+                            ));
+
+                
+                
+                
+                $form->add('iso', 
+                    TextType::class, 
+                    array(
+                        "attr" => array
+                        ('class' => 'form-control col-md-7 col-xs-12', 'placeholder'=>false)));
+                
+                
+                
+                $form->add('banderapais', FileType::class, array(
+                    'label' => 'Bandera:', 
+                    'mapped'=> false,
+                    'required'   => true, 
+                    "attr" => array('accept'=>'application/png'),
+                    'data_class' => null));
+                     
+            $form->add('guardar', SubmitType::class, 
+                    array('label' => 'Registrar', 
+                        "attr" => array('class' => 'btn btn-success')));
+        
+        $form = $form->getForm();
+            
         $form->handleRequest($request);
         // replace this example code with whatever you need
         if ($form->isSubmitted() && $form->isValid()) {
@@ -54,7 +127,7 @@ class CodigosController extends Controller
         
         
 
-        return $this->redirectToRoute('paises_listado_general');
+        return $this->redirectToRoute('codigos_listado_general');
     }
          return $this->render('AppBundle:Codigos:agregar-nuevo-pais.html.twig', array('form'=> $form->createView()));
     
@@ -62,7 +135,7 @@ class CodigosController extends Controller
     }
     
     /**
-     * @Route("/paises/ver-informacion/", name="paises_ver_informacion")
+     * @Route("/paises/ver-informacion/codigos/", name="codigos_paises_ver_informacion")
      */
     public function VerPaisAction(Request $request)
     {
